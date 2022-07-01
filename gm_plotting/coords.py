66cb386acd6f6d0b0d38649bb133bf2409e8ce54
Alex Dewar <a.dewar@imperial.ac.uk>
2022-07-01 14:45:01 +0100

    Fix conversion routines for non-numpy array inputs

--- gm_plotting/coords.py
+++ gm_plotting/coords.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 import numpy as np
 
 # Optional support for converting from UTM coordinates
@@ -8,11 +10,19 @@ except:
 
 
 def gps_to_merc(lat, lon):
+    if isinstance(lat, Iterable):
+        lat = np.array(lat)
+        lon = np.array(lon)
+
     mlat = 0.5 - np.log(np.tan(np.deg2rad(45 + lat / 2))) / (2 * np.pi)
     mlon = 0.5 + lon / 360
     return mlat, mlon
 
 def merc_to_gps(mlat, mlon):
+    if isinstance(mlat, Iterable):
+        mlat = np.array(mlat)
+        mlon = np.array(mlon)
+
     lat = np.rad2deg(2 * np.arctan(np.exp(np.pi * (1 - 2 * mlat)))) - 90
     lon = 360 * (mlon - 0.5)
     return lat, lon
