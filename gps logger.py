8c4a1593bfc18a4e7834336bf548a7e1bad7f611
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2022-08-24 09:54:27 +0100

    raw file

--- gps logger.py
+++ gps logger.py
@@ -10,7 +10,6 @@ def serial_ports():
         Read the all ports that are open for serial communication
         @returns array of COMS avaliable
         """
-
         if sys.platform.startswith('win'):
             ports = ['COM%s' % (i + 1) for i in range(256)]
         elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
@@ -27,7 +26,7 @@ def serial_ports():
                 s = serial.Serial(port)
                 s.close()
                 result.append(port)
-            except:
+            except (OSError, serial.SerialException):
                 pass
         return result
     
@@ -37,28 +36,27 @@ def converter_A(cord):
     minutes=float(cord[2:8]) #MM.MMMM
     decimal = degrees + (minutes/60)
     return decimal
+
 def converter_B(cord):
     #DDDMM.MMMMM
     degrees=float(cord[0:3]) #DDD
     minutes=float(cord[3:9]) #MM.MMMM
     decimal = degrees + (minutes/60)
     return decimal
-ser=None
+
 con=False
 while not con: #loop till connection established
     try:
         print(serial_ports())
-        s=input("Enter your serial port: ")
-        ser=serial.Serial(s)
-        con=True
-    except KeyboardInterrupt:
-        exit()
+        serial=input("Enter your serial port: ")
+        ser=serial.Serial(serial)
     except:
         pass
     
 ser.baudrate=4800
 name=input("What would you like to name your file? ")
 f = open(name+'.csv', 'w')
+f1 = open("debug "+name+'.txt', 'w')
 print('Waiting for data')
 while True:
     try:     
@@ -84,10 +82,11 @@ while True:
                     if gpgga.lon_direction == "W":
                         lon*=-1
                     #print("{},{},{},{}\n".format(current_time,gpgga.latitude, gpgga.longitude,gpgga.num_sats))
-                    f.write("{},{},{},{}\n".format(current_time,lat,lon,gpgga.num_sats))
-                    
+                    f.write("{},{:.20f},{:.20f},{}\n".format(current_time,lat,lon,gpgga.num_sats))
+                    f1.write(message+"\n")
 
     except KeyboardInterrupt:
         raise
 f.close()
+f1.close()
 ser.close()
